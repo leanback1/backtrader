a146cb727258f84a757c02eb5df2660a5d5515be
aakva <aakva@equinor.com>
2021-10-18 08:56:16 +0200

    Added RSI indicator to SMA strategy

--- run_strategy.py
+++ run_strategy.py
@@ -1,4 +1,6 @@
-import os, sys, argparse
+import os
+import sys
+import argparse
 from backtrader import indicator
 import pandas as pd
 import backtrader as bt
@@ -15,21 +17,30 @@ from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
 strategies = {
-        "golden_cross": GoldenCross,
-        "buy_hold": BuyHold,
-        "ma_cross": MAcrossover,
-        "atr": AverageTrueRange,
-        "sma": SmaCross,
-        "donchian": DonchianChannelsStr,
-        "mfi": MoneyFlowStr
-    }
+    "golden_cross": GoldenCross,
+    "buy_hold": BuyHold,
+    "ma_cross": MAcrossover,
+    "atr": AverageTrueRange,
+    "sma": SmaCross,
+    "donchian": DonchianChannelsStr,
+    "mfi": MoneyFlowStr
+}
 
-def get_data(ticker):        
-    dateToday = str(int(time.time()))    
-    tmpDateOneYearAgo = datetime.now() - relativedelta(years=2)
-    dateOneYearAgo = str(int(time.mktime(tmpDateOneYearAgo.timetuple())))
-    url = 'https://query1.finance.yahoo.com/v7/finance/download/'+ ticker + '?period1=' + dateOneYearAgo +'&period2='+ dateToday +'&interval=1d&events=history&includeAdjustedClose=true'
-    return pd.read_csv(url, index_col='Date',parse_dates=True)
+
+def get_data(ticker):
+    df = None
+    try:
+        dateToday = str(int(time.time()))
+        tmpDateOneYearAgo = datetime.now() - relativedelta(years=2)
+        dateOneYearAgo = str(int(time.mktime(tmpDateOneYearAgo.timetuple())))
+        url = 'https://query1.finance.yahoo.com/v7/finance/download/' + ticker + '?period1=' + \
+            dateOneYearAgo + '&period2=' + dateToday + \
+            '&interval=1d&events=history&includeAdjustedClose=true'
+        df = pd.read_csv(url, index_col='Date', parse_dates=True)
+    except Exception as e:  # proxy issues
+        df = pd.read_csv('data/DNO.OL.csv', index_col='Date', parse_dates=True)
+    return df
+
 
 def main():
     # parse command line arguments
@@ -44,16 +55,17 @@ def main():
     cerebro = Cerebro()
     cerebro.broker.setcash(250000)
     cerebro.addsizer(bt.sizers.AllInSizer, percents=90)
-    cerebro.addstrategy(MoneyFlowStr)
+    # cerebro.addstrategy(MoneyFlowStr)
     cerebro.addstrategy(strategy=strategies[args.strategy])
-    cerebro.adddata(bt.feeds.PandasData(dataname=get_data('EQNR.OL')))    
+    cerebro.adddata(bt.feeds.PandasData(dataname=get_data('EQNR.OL')))
 
     cerebro.addwriter(bt.WriterFile, csv=True, out='log.csv')
     print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
     cerebro.run()
     print('Ending Portfolio Value: %.2f' % cerebro.broker.getvalue())
-    
+
     cerebro.plot()
+
 
 if __name__ == "__main__":
     main()
--- strategies/SmaCross.py
+++ strategies/SmaCross.py
@@ -1,8 +1,12 @@
 import backtrader as bt
+from indicators.MoneyFlowInd import MFI
+
 
 class SmaCross(bt.SignalStrategy):
-  def __init__(self):
-    sma = bt.ind.SMA(period=50)
-    price = self.data
-    crossover = bt.ind.CrossOver(price,sma)
-    self.signal_add(bt.SIGNAL_LONG, crossover)
+    def __init__(self):
+        sma = bt.ind.SMA(period=50)
+        price = self.data
+        crossover = bt.ind.CrossOver(price, sma)
+        self.signal_add(bt.SIGNAL_LONG, crossover)
+        self.myind1 = MFI(self.data)
+        self.rsi = bt.ind.RSI(period=14)
